3e94e282df71f3b7d46f10b5097e33d92794413f
DLP-Sekara <dslahirupiyumal99@gmail.com>
2023-05-30 15:31:58 +0530

    dashboard view created

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import * as React from 'react';
+//import {NavigationContainer} from '@react-navigation/native';
+//import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Dashboard from './src/pages/Dashboard/Dashboard';
 
-const App = () => {
-  return <Dashboard />;
-};
+//const Stack = createNativeStackNavigator();
 
+const App = () => {
+  return (
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Profile" component={Dashboard} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <Dashboard />
+  );
+};
 export default App;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,32 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 
 const Dashboard = () => {
+  function checkData(): void {
+    console.log('first');
+  }
+
   return (
-    <View>
-      <Text style={styles.text}>Dashboard</Text>
+    <View style={styles.background}>
+      <View style={styles.textContainer}>
+        <Text style={styles.textOne}>CAR</Text>
+        <Text style={styles.textTwo}>Connect</Text>
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.descText}>
+          Best Car Buying And Selling Platform
+        </Text>
+      </View>
+      <View style={styles.imgArea}>
+        <Image
+          style={styles.img}
+          source={require('../../assets/Images/cars/v12.png')}
+        />
+      </View>
+      <TouchableOpacity style={styles.btn} onPress={checkData}>
+        <Text style={{color: 'black', fontSize: 20}}>Explore Now</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -12,7 +34,47 @@ const Dashboard = () => {
 export default Dashboard;
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'green',
+  background: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  textContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    top: '30%',
+  },
+  textOne: {
+    color: '#EEBC4F',
+    fontSize: 36,
+  },
+  textTwo: {
+    color: '#8A7D7D',
+    fontSize: 36,
+  },
+  descText: {
+    flexDirection: 'row',
+    top: 30,
+    color: '#8A7D7D',
+    fontSize: 18,
+  },
+  imgArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  img: {
+    marginLeft: 50,
+    height: 250,
+    width: 450,
+  },
+  btn: {
+    width: '60%',
+    backgroundColor: '#DFAD41',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+    marginLeft: '20%',
+    bottom: '10%',
   },
 });
